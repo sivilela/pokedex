209d7a3e884b5a78a75dbc246f5a98ae67fbbcdf
sivilela <sivilela.s@gmail.com>
2023-11-11 17:39:52 -0300

    Criado a estrutura de arquivos e componentes

---
+++ pokedex/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "./Components/Header";
+import { PokemonCard } from "./Components/PokemonCard"
+
+const App = () => {
+  return (
+    <Header>
+    </Header>
+  
+  );
+}
+
+export default App;
